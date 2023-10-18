c98d8bcda69a4a130e052fa7b5485d86839cbae5
haeunchoi-dev <haeunchoi.dev@gmail.com>
2023-10-18 23:44:56 +0900

    feat : 채용공고 상세페이지 조회시 회사가올린다른채용공고도 조회

--- src/main/java/eun/wanted/preonboarding/dto/NoticeDetailDto.java
+++ src/main/java/eun/wanted/preonboarding/dto/NoticeDetailDto.java
@@ -1,6 +1,8 @@
 package eun.wanted.preonboarding.dto;
 
 import eun.wanted.preonboarding.entity.Notice;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +19,7 @@ public class NoticeDetailDto {
     private String company;
     private String region;
     private String contry;
+    private List<Long> noticeIds;
 
     public NoticeDetailDto(final Notice entity) {
         this.id = entity.getId();
@@ -27,5 +30,7 @@ public class NoticeDetailDto {
         this.company = entity.getCompany().getName();
         this.region = entity.getCompany().getRegion().getName();
         this.contry = entity.getCompany().getRegion().getContry().getName();
+        this.noticeIds = entity.getCompany().getNotices().stream().map(Notice::getId).collect(
+            Collectors.toList());
     }
 }
